79d6ba8060cdf8e00f8b9fe03414e5003af08eb0
nithesh407 <nitheshravikumar13631@gmail.com>
2022-01-23 13:33:53 +0530

    to print matrix and their sum

--- array.c
+++ array.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 int main()
 {
-   int a[5],i;
+   int a[5];
    printf("Enter the values: ");
-   for(i=0;i<5;i++)
-       scanf("%d",&a[i]);
-   for(i=0;i<5;i++) 
+   for(int i=0;i<5;i++)
+   {
+        scanf("%d",&a[i]);
+   }
+   for(int i=0;i<5;i++)
+   {
         printf("\narray element in index %d : %d",i,a[i]);
-   for(i-4;i>=0;i--)
+   }
+   for(int i= a[i]5-1;i!=-1;i--)
+   {     
         printf("\narray element in index %d : %d",i,a[i]);
+   }
 }
